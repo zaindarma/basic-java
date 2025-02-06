efc78e2c79afd77ae94e8002c42452bf3ecdf2fe
zainDarma <darma.nh34@gmail.com>
2025-02-06 17:09:38 +0700

    Test

--- src/day2/OOP/Test/Main.java
+++ src/day2/OOP/Test/Main.java
@@ -7,6 +7,8 @@ package day2.OOP.Test;
 * 2. Cari nilai tertinggi
 * 3. dan tampilkan data Student dengan nilai diatas rata-rata*/
 
+import day2.OOP.Student.Student;
+
 import java.util.ArrayList;
 
 public class Main {
@@ -16,10 +18,23 @@ public class Main {
         student.add(new Students("Zain", 001, 90));
         student.add(new Students("Zaun", 002, 80));
         student.add(new Students("Zaen", 003, 70));
+        student.add(new Students("Zaon", 004, 95));
 
-        // Panggil method
-        double avg = cekRataRata(student);
+        // Panggil method average
+        System.out.println("=== NO.1 ===");
+        int avg = cekRataRata(student);
         System.out.println("Rata-rata : " + avg);
+
+        // Panggil method max
+        System.out.println("=== NO.2 ===");
+        int max = cekNilaiTertinggi(student);
+        System.out.println("Nilai tertinggi : " + max);
+
+        // Panggil
+        System.out.println("=== NO.3 ===");
+        cekNilaiDiatas(student);
+
+
     }
 
     // Method Average
@@ -29,6 +44,27 @@ public class Main {
             nilaiTotal += students.nilai;
         }
         return nilaiTotal/ student.size();
+    }
+
+    // Method Nilai Tertinggi
+    public static int cekNilaiTertinggi(ArrayList<Students> student){
+        int nilaiTertinggi = 0;
+        for (Students students : student){
+            if (students.nilai > nilaiTertinggi){
+                nilaiTertinggi = students.nilai;
+            }
+        }
+        return nilaiTertinggi;
+    }
+
+    // Method Nilai Diatas Rata2
+    public static void cekNilaiDiatas(ArrayList<Students> student){
+        for (Students students : student){
+            if (students.nilai > cekRataRata(student)){
+                students.displayStudent();
+            }
+        }
+
     }
 
 }
